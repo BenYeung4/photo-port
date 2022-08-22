d51bb75432b425e3c16e860eefdc41ab1c2ff23e
Ben <byeungis@gmail.com>
2022-08-22 00:27:57 -0400

    updated About & Nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+//the html under the funciton is actually called JSX, represents HTML in Javascript.
+//categories, we using the .map() function, defining the categories in an array above the return statment.  Creating objects that contain each category's na e and short description helps when we use the same data elswehere in the app.
+function Nav() {
+  const categories = [
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
+  //returning the below's return
+  function categorySelected(name) {
+    console.log(`${name} clicked`);
+  }
+
+  return (
+    <header>
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Nav;
