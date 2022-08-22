8868644905c14fdb1e1e63cec6c6cba3e8b4057f
Ben <byeungis@gmail.com>
2022-08-21 23:25:39 -0400

    initial react works, until removing logo.sv and app.test.js file

---
+++ note.md
@@ -0,0 +1,9 @@
+Start off with : npx create-react-app photo-port
+
+npx - package runner tool simplifies the installation and runing process of devDependencies. Rather than installing CRA and then running i, npx will try using the local installation, if there isnt any, npx will install it for us.
+
+create-react-app - is the created source code, photo-port is just the name
+
+npm install the updated package and npm start
+
+react-dom package - serves as the entry point to the DOM to render the JSX by using the virtual DOM.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//index.js is main entry point for the application
+//The ReactDOM library is rendering the App component at the root element in the HTML. You can find the HTML by navigating to the public folder. Under public/index.html the "root", is where React will inject the whole app into whichever element has the "root" id
+import React from "react";
+//replace ReactDOM replace with react-dom instead of react-dom/client"new version"
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
+
+//version 18.2.0
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
